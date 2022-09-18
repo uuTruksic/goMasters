51f4a07a2f5cd7700e1cf53cc1f20b7ec62df304
Vojtěch Novotný <vojta.nvtn@gmail.com>
2022-09-18 18:30:31 +0200

    Fiber

--- api/main.go
+++ api/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Hello")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
