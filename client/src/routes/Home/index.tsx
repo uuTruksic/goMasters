a8903007d6f821e2de3f5478dd54ea4eab2ae8e9
Michal <michtruksa@gmail.com>
2022-09-20 22:50:37 +0200

    fe

--- client/src/routes/Home/index.tsx
+++ client/src/routes/Home/index.tsx
@@ -1,17 +1,16 @@
-import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { Container } from "./styled";
+import { Container, StyledButton } from "./styled";
 
 const Home = () => {
   const navigate = useNavigate();
   return (
     <Container>
-      <Button variant="contained" onClick={() => navigate("/register")}>
+      <StyledButton variant="contained" onClick={() => navigate("/register")}>
         Register
-      </Button>
-      <Button variant="contained" color="secondary" onClick={() => navigate("/login")}>
+      </StyledButton>
+      <StyledButton variant="contained" color="secondary" onClick={() => navigate("/login")}>
         Login
-      </Button>
+      </StyledButton>
     </Container>
   );
 };
--- client/src/routes/Home/styled.ts
+++ client/src/routes/Home/styled.ts
@@ -1,10 +1,21 @@
+import { Button } from "@mui/material";
 import styled from "styled-components";
 
 export const Container = styled.div`
   width: 300px;
-  min-height: 100vh;
+  padding: 50px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   margin: 0 auto;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  border-radius: 20px;
+`;
+
+export const StyledButton = styled(Button)`
+  width: 100px !important;
 `;
