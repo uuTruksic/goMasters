ea7faad70a7542390754b1e12efaee2123ea2a93
michal truksa <michtruksa@gmail.com>
2022-09-27 15:31:17 +0200

    routes

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./routes/Home";
+import AllSongs from "./routes/AllSongs";
+import FavoriteSongs from "./routes/FavoriteSongs";
 
 const Router = () => (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/vsechny-pisnicky" element={<Home />} />
-    <Route path="/oblibene-pisnicky" element={<Home />} />
+    <Route path="/vsechny-pisnicky" element={<AllSongs />} />
+    <Route path="/oblibene-pisnicky" element={<FavoriteSongs />} />
   </Routes>
 );
 
