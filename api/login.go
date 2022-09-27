fa33f2d177c60a4b9122487a0803808778c1cc1b
Vojtěch Novotný <vojta.nvtn@gmail.com>
2022-09-27 16:10:01 +0200

    Backend

--- api/login.go
+++ api/login.go
@@ -6,7 +6,22 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type loginInput struct {
+	Email    string `json:"email" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
 func login(c *fiber.Ctx) error {
-	log.Println("ouhuihi")
-	return nil
+	data := loginInput{}
+	err := c.BodyParser(&data)
+	if err != nil {
+		log.Println(err)
+		return c.SendStatus(fiber.StatusBadRequest)
+	}
+
+	if data.Email == "hezky.email@seznam.cz" && data.Password == "heslo" {
+		return c.JSON(true)
+	} else {
+		return c.JSON(false)
+	}
 }
