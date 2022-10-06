013a7ac651922ec3085d611d5709b5ceade3eed7
Vojtěch Novotný <vojta.nvtn@gmail.com>
2022-10-06 23:26:48 +0200

    Backend

--- api/login.go
+++ api/login.go
@@ -3,12 +3,21 @@ package main
 import (
 	"api/ent/user"
 	"context"
-	"fmt"
+	"crypto/rand"
+	"encoding/hex"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 )
+
+func GenerateSecureToken(length int) string {
+	b := make([]byte, length)
+	if _, err := rand.Read(b); err != nil {
+		return ""
+	}
+	return hex.EncodeToString(b)
+}
 
 type loginInput struct {
 	Email    string `json:"email" validate:"required"`
@@ -29,16 +38,24 @@ func login(c *fiber.Ctx) error {
 		return c.SendStatus(fiber.StatusBadRequest)
 	}
 	if foundUser == nil {
-		fmt.Println("Wrong e-mail")
+		log.Println("Wrong e-mail")
 		return c.SendStatus(fiber.StatusBadRequest)
 	}
 
 	passCompare := bcrypt.CompareHashAndPassword([]byte(foundUser.Password), []byte(data.Password))
-	if passCompare == nil {
-		fmt.Println("login succesfull")
-		return c.SendStatus(fiber.StatusOK)
-	} else {
-		fmt.Println("Wrong password")
+	if passCompare != nil {
+		log.Println("Wrong password")
 		return c.SendStatus(fiber.StatusBadRequest)
 	}
+	token := GenerateSecureToken(64)
+
+	_, err = Client.Session.Create().SetIP(c.IP()).SetDevice(c.Get("user-agent")).SetToken(token).SetUsed(0).SetUser(foundUser).Save(c.Context())
+	if err != nil {
+		log.Println(err)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{
+		"token": token,
+	})
 }
